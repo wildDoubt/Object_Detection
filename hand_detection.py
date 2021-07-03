0d5d2a6d3e238bddc3ad4dcba64d850cc7fa15d6
Wonyeong Choi <coma01155@gmail.com>
2021-07-03 23:26:56 +0900

    optimize imports

--- hand_detection.py
+++ hand_detection.py
@@ -1,6 +1,3 @@
-import cv2
-import numpy as np
-import os
 from Video import *
 
 
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
 import re
-import cv2
-import numpy as np
-from os.path import isfile, join
-import matplotlib.pyplot as plt
 from Video import *
 
 # 동영상에서 자동차 인식
